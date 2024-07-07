53b235eeba4c6cca8181e0566d318c1c9ed79192
Pawan Bangar <pawanbangar5360@gmail.com>
2024-07-07 12:52:20 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        Count Down Timer
       </header>
+      <div className='flex gap-x-1 justify-center'>
+        <div className="flex-initial">Hours</div>
+        <div className="flex-initial">Minutes</div>
+        <div className="flex-initial">Seconds</div>
+      </div>
+      <div className='flex gap-x-1 justify-center'>
+        <input min={0} max={60} maxLength={2} className="flex-initial">Hours</input>
+        <input type="number" min={0} max={59} maxLength={2} className="flex-initial">Minutes</input>
+        <input min={0} max={59} maxLength={2} className="flex-initial">Seconds</input>
+      </div>
     </div>
   );
 }
